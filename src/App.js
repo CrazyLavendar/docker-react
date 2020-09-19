4dede073fa977e68bdc33a1462edfec67aa6ad09
Jayam Vimal <crazylavendar@gmail.com>
2020-09-19 12:06:07 +0530

    T2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          yooyo8yoyfjfrytuu6eujek8yoyufuyfuyfuyfuy
+          Learn React
         </a>
       </header>
     </div>
